e6401fc8e874498b2ceb27300d162d42b5da1342
Lam Nhan <lamnhan92@gmail.com>
2019-11-22 20:29:48 +0700

    update docs

--- ayedocs.config.js
+++ ayedocs.config.js
@@ -9,16 +9,6 @@ module.exports = {
       cleanOutput: false,
       rendering: {
         head: true,
-        tocx: true,
-        options: [
-          ['Options', 'SELF', { title: 'Options' }],
-          ['Options', 'SUMMARY_PROPERTIES', { heading: true }]
-        ],
-        main: ['Main', 'FULL', { title: 'The Module' }],
-        declaration: ['Declaration', 'FULL', { title: 'Declaration' }],
-        parser: ['ParseService', 'FULL', { title: 'Parsing' }],
-        converter: ['ConvertService', 'FULL', { title: 'Converting' }],
-        renderer: ['RenderService', 'FULL', { title: 'Rendering' }],
         license: true
       }
     }
